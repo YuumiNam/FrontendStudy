836ade6831718ea1099795e73fa49820be9b848d
dltjsam <dltjsam@gmail.com>
2023-02-14 12:38:02 +0900

    React #04

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -2,14 +2,24 @@
 import logo from './logo.svg';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import data from './data.js';
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route, Link, useParams, useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
 
 function ShoesList(props) {
+
+  const navigate = useNavigate();
+
   return (
     <div className="col-md-4">
-      <img src={'https://codingapple1.github.io/shop/shoes'+ props.i +'.jpg'} width="80%"></img>
+      <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) +'.jpg'}
+        width="80%" 
+        style={{cursor:'pointer'}}
+        onClick={() => {
+          navigate('/detail/' + props.i)
+        }}></img>
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
@@ -27,7 +37,7 @@ function MainPage() {
           {
             data.map(function(a, i) {
               return (
-                <ShoesList shoes={a} i={i+1} key={i}></ShoesList>
+                <ShoesList shoes={a} i={i} key={i}></ShoesList>
               )
             })
           }
@@ -37,17 +47,51 @@ function MainPage() {
   )
 }
 
-function SecondPage() {
+function SecondPage(props) {
+
+  // 라우터에서 썼던 url파라미터가 여기에 저장됨
+  let {id} = useParams();
+
+  let idNum = parseInt(id);
+
+  // LifeCycle
+  // 언제쓰면좋나?
+  // 1. 재렌더링마다 코드실행하고 싶으면  useEffect(() => {  })
+  // 2. mount시 1회 코드실행하고 싶으면  useEffect(() => {  }, [])
+  // 3. useEffect(() => { return () => { unmount시 1회코드실행하고싶으면 } }, [])
+  // 4. useEffect실행 전에 뭔가 실행하려면  언제나 return () => {}
+  // 5. 특정 state 변경시에만 실행하려면 [state명]
+  let [alert, setAlert] = useState(true);
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    console.log(2);
+
+    // useEffect 동작 전에 실행됨
+    return () => {
+      clearTimeout(a); // 타이머 초기화주는 함수임
+      console.log(1);
+    }
+  }, []); // [] 이렇게 해주면 mount시 한번만 실행됨
+
   return (
     <div className="container">
+      {
+        alert == true
+        ? <div className='alert alert-warning'>
+            2초 이내 구매시 할인
+          </div>
+        : null
+      }
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img src={"https://codingapple1.github.io/shop/shoes"+ (idNum+1) +".jpg"} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
@@ -65,10 +109,10 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/">
+            <Link to="/" style={{textDecoration: "none"}}>
               <Nav.Link href="#home">Home</Nav.Link>
             </Link>
-            <Link to="/detail">
+            <Link to="/detail/0" style={{textDecoration: "none"}}>
               <Nav.Link href="#features">Cart</Nav.Link>
             </Link>
           </Nav>
@@ -80,10 +124,21 @@ function App() {
             <MainPage></MainPage>
           }> 
         </Route>
-        <Route path='/detail' element={
-          <SecondPage></SecondPage>
+        <Route path='/detail/:id' element={
+          <SecondPage shoes={shoes}></SecondPage>
         }></Route>
       </Routes>
+
+      {/* AJAX AXIOS */}
+      <button onClick={() => {
+        axios.get('https://codingapple1.github.io/shop/data2.json')
+          .then((결과) => {
+            console.log(결과.data);
+          })
+          .catch(() => {
+            console.log('실패함ㅅㄱ');
+          })
+      }}>버튼</button>
     </div>
   );
 }
